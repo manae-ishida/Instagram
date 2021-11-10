9bdc99272725db024760e3eca22ec7c06e145184
manae_ishida <manae_ishida@irep.co.jp>
2021-11-10 20:54:30 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 石田 真苗 on 2021/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
